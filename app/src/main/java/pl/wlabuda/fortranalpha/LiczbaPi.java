799c292151c79fd3e66a99804a1a31fadad43fcd
SkylinR <skylinr7@gmail.com>
2016-02-17 20:41:29 +0100

    Push do pulla z nowego brancha z testem

--- app/src/main/java/pl/wlabuda/fortranalpha/LiczbaPi.java
+++ app/src/main/java/pl/wlabuda/fortranalpha/LiczbaPi.java
@@ -1,8 +1,8 @@
 package pl.wlabuda.fortranalpha;
 
 /**
- * Created by waclab1807 on 29.12.15.
- */
+ * Created by waclab1807 on 29.12.15. & SkylinR on 17.02.16
+*/
 public class LiczbaPi {
     public String first;
     public String valpi;
@@ -12,21 +12,33 @@ public class LiczbaPi {
             first = "1";
             valpi = "π";
         }
-        if(pi.contains("-π")) { //-pi => -1pi
-            pi = pi.replace("-π","-1π");
+
+        if(pi.contains("-π")) { //-π => -1π
+            //pi = pi.replace("-π","-1π");
+            first = "-1";
+            valpi="-π";
         }
+
+        if(pi.contains("π")&& pi.charAt(0)=='0' && pi.length()==2) { // 0π
+            first = "0";
+            valpi= "0";
+        }
+
         if(pi.endsWith("π") && pi.length() != 1 && !pi.substring(pi.length()-1,pi.length()).equals("/") && !pi.substring(pi.length()-1,pi.length()).equals("^") && !pi.substring(pi.length()-1,pi.length()).equals("√")) {
             first = pi.replace("π","");
             valpi = first + "π";
         }
+
         if(pi.contains("/π")) {         //
             first = pi;
             valpi = "(" + pi + ")";
         }
+
         if(pi.endsWith("π") && pi.substring(0,pi.length()-1).equals("/") && !pi.contains("/π")) {       // (3/4)π
             first = pi.substring(0,pi.length()-1);
             valpi = "(" + first + ")π";
         }
+
         if(pi.contains("π/")) {
             String[] tmp = pi.split("/");
             if(tmp[0].length()<2){
