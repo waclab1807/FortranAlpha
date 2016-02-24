668e9dce50716660a14d311e60f49f8750150d8d
SkylinR <skylinr7@gmail.com>
2016-02-24 22:03:23 +0100

    Remove comment

--- app/src/androidTest/java/pl/wlabuda/fortranalpha/ApplicationTest.java
+++ app/src/androidTest/java/pl/wlabuda/fortranalpha/ApplicationTest.java
@@ -2,7 +2,6 @@ package pl.wlabuda.fortranalpha;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-//COMENT penis BLA BLA BLA  I JESZCZE JEDNO BLA DLA DEVA
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
